e4c9fb02f425d8f5710901fec77617a45ee20c3c
kirtika-0102 <kirtikamittal0102@gmail.com>
2026-04-08 16:10:38 +0530

    Profile updated

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import streamlit as st
 st.title("Streamlit App")
 home_page= st.Page(
-    page= "views\home.py",
+    page= "views/home.py",
     title= "HOME",
     #icon= ":material\user:"
 )
 profile_page= st.Page(
-    page= "views\profile.py",
+    page= "views/profile.py",
     title= "PROFILE",
     #icon= ":material\user:",
     default= True
 )
 contact_page= st.Page(
-    page= "views\contact.py",
+    page= "views/contact.py",
     title= "CONTACT US",
     #icon= ":material\user:"
 )
@@ -21,6 +21,6 @@ nb= st.navigation({
     'Useful links':[contact_page]
     })
 
-st.logo("assets\logo.jpeg")
+st.logo("assets/logo.jpeg")
 st.sidebar.text("Powered by : Kirtika Mittal")
 nb.run()
